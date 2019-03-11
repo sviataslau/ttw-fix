f3ccb6e1f5d930f0122d5e11fbf28ea897048936
sviataslau <sviataslau@gmail.com>
2019-03-11 18:10:49 +0300

    1.0

---
+++ content.js
@@ -0,0 +1,11 @@
+﻿var $frame = $('#frame');
+
+function fix() {
+    var height = $(document.body).prop('scrollHeight');
+    $frame.height(height);
+}
+
+$(document).ready(function () {
+    var timeoutMs = 1000;
+    $frame.ready(setTimeout(fix, timeoutMs));
+});
